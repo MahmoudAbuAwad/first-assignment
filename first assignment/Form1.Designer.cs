ab6d2f93436a29f319b9585a805465046ad69a3b
mahmoud abuawad <ac0384@iu.edu.jo>
2022-06-01 15:41:49 +0300

    adding database

---
+++ first assignment/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace first_assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblusername = new System.Windows.Forms.Label();
+            this.lblpassword = new System.Windows.Forms.Label();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblusername
+            // 
+            this.lblusername.AutoSize = true;
+            this.lblusername.Location = new System.Drawing.Point(30, 41);
+            this.lblusername.Name = "lblusername";
+            this.lblusername.Size = new System.Drawing.Size(54, 13);
+            this.lblusername.TabIndex = 0;
+            this.lblusername.Text = "username";
+            // 
+            // lblpassword
+            // 
+            this.lblpassword.AutoSize = true;
+            this.lblpassword.Location = new System.Drawing.Point(30, 98);
+            this.lblpassword.Name = "lblpassword";
+            this.lblpassword.Size = new System.Drawing.Size(53, 13);
+            this.lblpassword.TabIndex = 1;
+            this.lblpassword.Text = "password\r\n";
+            // 
+            // txtusername
+            // 
+            this.txtusername.Location = new System.Drawing.Point(108, 38);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(146, 20);
+            this.txtusername.TabIndex = 2;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(108, 98);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(146, 20);
+            this.txtpassword.TabIndex = 3;
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnlogin.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnlogin.Location = new System.Drawing.Point(131, 142);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(75, 23);
+            this.btnlogin.TabIndex = 4;
+            this.btnlogin.Text = "login";
+            this.btnlogin.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(329, 194);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtusername);
+            this.Controls.Add(this.lblpassword);
+            this.Controls.Add(this.lblusername);
+            this.Name = "Form1";
+            this.Text = "login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblusername;
+        private System.Windows.Forms.Label lblpassword;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.Button btnlogin;
+    }
+}
+
